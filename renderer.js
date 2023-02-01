d7277468189502d2ffd0d24d158c15c580f2a859
theripper93 <tsplab@gmail.com>
2023-02-01 18:34:30 +0100

    a

--- renderer.js
+++ renderer.js
@@ -16,17 +16,28 @@ document.querySelector("#add-game").addEventListener("click", () => {
     createGameList();
 });
 
-function createGameList() {
+async function createGameList() {
+    let config = {games: []};
+    try {
+        config = await fetch("config.json").then((res) => res.json());
+    }catch(e) {
+        console.log("Failed to load config.json");
+    }
+    if (config.background) document.body.style.backgroundImage = `url(${config.background})`;
+    if (config.textColor) document.documentElement.style.setProperty("--color-text-primary", config.textColor);
+    if (config.backgroundColor) document.documentElement.style.setProperty("--color-background", config.backgroundColor);
+    if(config.accentColor) document.documentElement.style.setProperty("--color-accent", config.accentColor);
     const ul = document.querySelector("#game-list");
     ul.innerHTML = "";
     const gameList = window.localStorage.getItem("gameList") || "[]";
-    const gameListJson = JSON.parse(gameList);
+    let gameListJson = JSON.parse(gameList);
+    gameListJson = [...config.games, ...gameListJson]
     gameListJson.forEach((game) => {
         const li = document.createElement("li");
         li.classList.add("game-item");
-        li.innerHTML = `<a href="${game.url}">${game.name}</a><button data-game-id="${game.id}" class="delete-game">X</button>`;
+        li.innerHTML = `<a href="${game.url}">${game.name}</a>${game.id ? `<button data-game-id="${game.id}" class="delete-game">X</button>` : ""}`;
         ul.appendChild(li);
-        li.querySelector(".delete-game").addEventListener("click", () => {
+        li.querySelector(".delete-game")?.addEventListener("click", () => {
             const gameList = window.localStorage.getItem("gameList") || "[]";
             const gameListJson = JSON.parse(gameList);
             const newGameList = gameListJson.filter((g) => g.id !== game.id);
