1ed7c709dd7be63d030c869fd577fb20c44adacf
theripper93 <tsplab@gmail.com>
2023-02-01 16:26:14 +0100

    a

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-const {app, BrowserWindow} = require("electron");
+const { app, BrowserWindow, globalShortcut } = require("electron");
 const path = require("path");
 
 app.commandLine.appendSwitch("force_high_performance_gpu", "true");
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    show: false,
-    width: 800,
-      height: 600,
-      webPreferences: {
-          preload: path.join(__dirname, "preload.js"),
-      },
-  });
-  win.loadFile("index.html");
-  win.maximize();
-  win.show();
+    const win = new BrowserWindow({
+        show: false,
+        width: 800,
+        height: 600,
+        nodeIntegration: false,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+    win.menuBarVisible = false;
+    win.loadFile("index.html");
+    win.maximize();
+    win.show();
 };
 
 app.whenReady().then(() => {
