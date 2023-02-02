a8c43c193015b547f61964711e994c7aba04d66f
theripper93 <tsplab@gmail.com>
2023-02-02 16:57:12 +0100

    a

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
-const { app, BrowserWindow, globalShortcut } = require("electron");
+const { app, BrowserWindow, globalShortcut, ipcMain, safeStorage } = require("electron");
 const path = require("path");
+const fs = require("fs");
 
 app.commandLine.appendSwitch("force_high_performance_gpu");
 
+let win;
+
+let gameId;
+
 const createWindow = () => {
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         show: false,
         width: 800,
         height: 600,
-        nodeIntegration: false,
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),
+            nodeIntegration: false,
+            contextIsolation: true,
+            enableRemoteModule: false,
         },
     });
-    win.menuBarVisible = false;
+    //win.menuBarVisible = false;
     win.loadFile("index.html");
     win.maximize();
     win.show();
@@ -21,4 +28,65 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
     createWindow();
+    win.webContents.on("did-finish-load", () => {
+        const userData = getLoginDetails(gameId);
+        if(!userData.user) return;
+            win.webContents.executeJavaScript(`
+
+        async function waitForLoad(){
+            const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+            while(!document.querySelector('select[name="userid"]')){
+                await wait(100);
+            }
+            login();
+        }
+
+        function login(){
+            const select = document.querySelector('select[name="userid"]');
+            select.querySelectorAll("option").forEach(opt => {
+                opt.selected = opt.innerText === "${userData.user}";
+            });
+            document.querySelector('input[name="password"]').value = "${userData.password}";
+            const fakeEvent = {preventDefault: ()=>{}, target: document.getElementById("join-game")}
+            ui.join._onSubmit(fakeEvent);
+        }
+
+        waitForLoad();
+
+        `);
+    });
 });
+
+ipcMain.on("open-game", (e, gId) => {
+    gameId = gId;
+});
+
+ipcMain.on("save-user-data", (e, data) => {
+    const {gameId, password, user} = data;
+    saveUserData(gameId, { password: Array.from(safeStorage.encryptString(password)), user });
+});
+
+function getUserData() {
+    try {
+        const json = fs.readFileSync(path.join(app.getPath("userData"), "userData.json"));
+        return JSON.parse(json);
+    } catch (e) {
+        return {};
+    }
+}
+
+function getLoginDetails(gameId) {
+    const userData = getUserData()[gameId];
+    if (!userData) return {user: "", password: ""};
+    const password = new Uint8Array(userData.password);
+    return {
+        user: userData.user,
+        password: safeStorage.decryptString(password),
+    };
+}
+
+function saveUserData(gameId, data) {
+    const currentData = getUserData();
+    const newData = { ...currentData, [gameId]: data };
+    fs.writeFileSync(path.join(app.getPath("userData"), "userData.json"), JSON.stringify(newData));
+}
--- preload.js
+++ preload.js
@@ -1,10 +1,29 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
+const { contextBridge, ipcRenderer } = require("electron");
 
-  for (const dependency of ["chrome", "node", "electron"]) {
-    replaceText(`${dependency}-version`, process.versions[dependency]);
-  }
+window.addEventListener("DOMContentLoaded", () => {
+    const replaceText = (selector, text) => {
+        const element = document.getElementById(selector);
+        if (element) element.innerText = text;
+    };
+
+    for (const dependency of ["chrome", "node", "electron"]) {
+        replaceText(`${dependency}-version`, process.versions[dependency]);
+    }
+});
+
+contextBridge.exposeInMainWorld("api", {
+    send: (channel, data) => {
+        // whitelist channels
+        let validChannels = ["toMain", "open-game", "save-user-data"];
+        if (validChannels.includes(channel)) {
+            ipcRenderer.send(channel, data);
+        }
+    },
+    receive: (channel, func) => {
+        let validChannels = ["fromMain", "save-user-data"];
+        if (validChannels.includes(channel)) {
+            // Deliberately strip event as it includes `sender`
+            ipcRenderer.on(channel, (event, ...args) => func(...args));
+        }
+    },
 });
