6b5cdaa251a4829a8491ca7a72e33500ccc66722
OmegaRogue <omegarogue@omegavoid.codes>
2023-08-12 18:12:18 +0200

    Add Name for windows shortcut

--- forge.config.js
+++ forge.config.js
@@ -4,7 +4,9 @@ module.exports = {
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        title: "VTT Desktop Client"
+      },
     },
     {
       name: '@electron-forge/maker-zip',
